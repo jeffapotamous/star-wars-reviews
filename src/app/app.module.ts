3429182963fd4180f18ef9ef18e499dfd76a887c
Jeffrey Buehler <jeffreydeanbuehler@gmail.com>
2020-11-02 14:51:26 -0700

    created page components, setup initial layout on app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BooksComponent } from './books/books.component';
+import { CloneWarsReviewComponent } from './clone-wars-review/clone-wars-review.component';
+import { CloneWarsChronologicalComponent } from './clone-wars-chronological/clone-wars-chronological.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BooksComponent,
+    CloneWarsReviewComponent,
+    CloneWarsChronologicalComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/clone-wars-chronological/clone-wars-chronological.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-clone-wars-chronological',
+  templateUrl: './clone-wars-chronological.component.html',
+  styleUrls: ['./clone-wars-chronological.component.css']
+})
+export class CloneWarsChronologicalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

6598f46ef9fc13ffa5bdb70fb01227e2f7d217b0
Jeffrey Buehler <jeffreydeanbuehler@gmail.com>
2020-11-06 22:04:09 -0700

    header move visible, lightsaber animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,18 +10,30 @@ import { DatabaseService } from './services/database.service';
 export class AppComponent implements OnInit {
 
   public loading: boolean;
+  public previewHeader: boolean;
+  public previewHeaderInterrupted: boolean;
   private currentCount: number;
   private totalFilesToLoadCount: number;
 
-  constructor(
-    private databaseService: DatabaseService
-  ) {
+  constructor(private databaseService: DatabaseService) {
     this.loading = true;
+    this.previewHeader = false;
+    this.previewHeaderInterrupted = false;
     this.currentCount = 0;
     this.totalFilesToLoadCount = 5;
   }
 
   ngOnInit() {
+    setTimeout(() => {
+      if (!this.previewHeaderInterrupted) {
+        this.previewHeader = true;
+      }
+      setTimeout(() => {
+        if (!this.previewHeaderInterrupted) {
+          this.previewHeader = false;
+        }
+      }, 1000);
+    }, 2000);
     this.databaseService.loadBooks();
     this.databaseService.loadCloneWarsEpisodes();
     this.databaseService.loadTags();

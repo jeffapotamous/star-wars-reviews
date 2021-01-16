75ba957bd09e497ce062d60936aa897fd2db339f
Jeffrey Buehler <jeffreydeanbuehler@gmail.com>
2021-01-16 13:22:43 -0700

    added 2 more in-progress boks

--- src/app/services/book.service.ts
+++ src/app/services/book.service.ts
@@ -28,6 +28,26 @@ export class BookService {
         pros: "",
         cons: "",
         hasCharacterSection: true
+      },
+      {
+        id: 16,
+        name: "Thrawn Ascendancy: Chaos Rising",
+        appleLink: "",
+        fileName: "Chaos Rising.jpg",
+        amazonLink: "https://www.amazon.com/dp/0593157680",
+        googlePlayLink: "https://play.google.com/store/books/details?id=OsmyDwAAQBAJ",
+        pros: "",
+        cons: ""
+      },
+      {
+        id: 17,
+        name: "Alphabet Squadron: Shadow Fall",
+        appleLink: "",
+        fileName: "shadow fall.jpg",
+        amazonLink: "https://www.amazon.com/dp/1984820044",
+        googlePlayLink: "https://play.google.com/store/books/details?id=58WyDwAAQBAJ",
+        pros: "",
+        cons: ""
       }
     ];
   }

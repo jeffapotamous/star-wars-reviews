a4add340e8c683fd2069c67a825f5b9a949dd70e
Jeffrey Buehler <jeffreydeanbuehler@gmail.com>
2021-01-16 16:42:18 -0700

    character content

--- src/app/services/book-character.service.ts
+++ src/app/services/book-character.service.ts
@@ -34,6 +34,8 @@ export class BookCharacterService {
             { name: 'Rah Barocci', gender: Gender.M },
             { name: 'Yarael Poof', gender: Gender.M, species: 'Quermian', photo: 'YaraelPoof-SWCT.png' },
             { name: 'Jora Malli', gender: Gender.F, species: 'Togruta', description: 'Bright orange skin and striking white facial markings.' },
+            { name: 'Zaviel Tepp' },
+            { name: 'Arkoff' }
           ]
         },
 
@@ -44,7 +46,10 @@ export class BookCharacterService {
             { name: 'Lourna Dee', gender: Gender.F, species: "Twi'lek", description: '40 years old, thin, green, emaciated lekku with bone white stripes. Armored leather from kell dragon, blue eyes. Subtle, introverted, keeping her intentions close until the result was achieved. Generally the cruelest of the 3.', photo: 'LournaDee.jpg' },
             { name: 'Kassav Millik', gender: Gender.M, species: 'Weequay', description: 'Aged, skin like sun-dried meat, fur cape. Chaotic and impulsive.' },
             { name: 'Marchion Ro', gender: Gender.M, description: 'gray skin, wholly black eyes, predator like. Son of Asgar Ro, former Eye of Nihil', photo: 'Marchion-ro-concept-art.jpg' },
-            { name: 'Zagyar', gender: Gender.M, description: 'Part of Lourna\'s crew' }
+            { name: 'Zagyar', gender: Gender.M, description: 'Part of Lourna\'s crew' },
+            { name: 'Gravhan', gender: Gender.M, species: 'Chevin', description: 'One of Kassav\'s Storm. Had tusks, one huge head to look at. Wrinkled gray skin and wisps of long blond hair.' },
+            { name: 'Dellex', gender: Gender.F, description: 'One of Kassav\'s Storm. One organic eye. Had many mechanical upgrades since she considered herself ugly.' },
+            { name: 'Wet Bub', gender: Gender.M, species: 'Gungan', description: 'One of Kassav\'s Storm. Slicer.' }
           ]
         },
 
@@ -69,10 +74,10 @@ export class BookCharacterService {
             { name: 'Petty Officer Innamin', gender: Gender.M, species: 'Human', description: 'Young (20s)' },
             { name: 'Captain Finial Bright', gender: Gender.M, species: 'Nautolan', description: 'Green skin, big black eyes. Tentacles' },
             { name: 'Ensign Peeples', gender: Gender.M, description: 'Needle like snout, 19 eyes' },
-            { name: 'Joss Adren', gender: Gender.M, species: '', description: 'Married to Pikka. Looked "like an engine block with a head stuck on top, with hair he kept cropped tight"' },
+            { name: 'Joss Adren', gender: Gender.M, species: '', description: 'Married to Pikka. Looked "like an engine block with a head stuck on top, with hair he kept cropped tight". Or "like a tree trunk with a head on top".' },
             { name: 'Pikka Adren', gender: Gender.F, species: 'Human', description: 'light eyes, dark curly hair, a pile of dark freckles, light skin' },
             { name: 'Odabba', species: 'Shistavanem', gender: Gender.M },
-            { name: 'Mari San Tekka', gender: Gender.F, species: 'Human', description: 'Over a century old, running on medical support. Pilot of the Gaze Electric, Marchion Ro\'s ship' }
+            { name: 'Mari San Tekka', gender: Gender.F, species: 'Human', description: 'Over a century old, running on medical support. Pilot of the Gaze Electric, Marchion Ro\'s ship' },
           ]
         },
 
@@ -94,6 +99,10 @@ export class BookCharacterService {
             { name: 'Larence Garello', gender: Gender.M, description: 'Chief executive of Garello Tech' },
             { name: 'Marlowe San Tekka', gender: Gender.M, species: 'Human', description: 'Scieon of the San Tekka empire, married to Vellis. Pale, blonde hair, blue eyes. Softer looking face.' },
             { name: 'Vellis San Tekka', gender: Gender.M, species: 'Human', description: 'Member of the San Tekka empire, married to Marlowe. Pale, blonde hair, blue eyes. Face looked chopped from granite.' },
+            { name: 'Ottoh Blythe', gender: Gender.M, species: 'Human', description: 'Farmer on Elphrona.' },
+            { name: 'Erika Blythe', gender: Gender.F, species: 'Human' },
+            { name: 'Ronn Blythe', gender: Gender.M, species: 'Human' },
+            { name: 'Bailen Blythe', gender: Gender.F, species: 'Human', label1: 'AKA: Bee' }
           ]
         }
       ],
@@ -103,7 +112,7 @@ export class BookCharacterService {
           label: 'Chiss',
           characters: [
             { name: 'Mitth\'raw\'nuruodo', gender: Gender.M, species: 'Chiss', label1: 'Core Name: Thrawn', label2: 'Birth Name: Kivu\'raw\'nuru (Vurawn)', photo: 'thrawn.png' },
-            {name: 'Ar\'alani', gender: Gender.F, species: 'Chiss', label1: 'Core Name: Ziara', label2: 'Birth Name: Irizi\'ar\'alani', photo: 'aralani.png'}
+            { name: 'Ar\'alani', gender: Gender.F, species: 'Chiss', label1: 'Core Name: Ziara', label2: 'Birth Name: Irizi\'ar\'alani', photo: 'aralani.png' }
           ]
         }
       ]
